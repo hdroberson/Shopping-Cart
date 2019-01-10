51e0022aadbe93aa2a71c3c53aa4593843434bb7
Harold Roberson <hdroberson@gmail.com>
2019-01-10 14:01:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,92 @@
-// create cart with array of Item (name, price, quantity)
+// add item to cart button
+var addButton = document.getElementById("add-button");
+addButton.addEventListener( "click", addItemToCart);
+
+// clear cart button
+var btn = document.getElementById("clear-cart");
+btn.addEventListener( "click", clearCart);
+
+
+
+
+// Shopping Cart
 var cart = [];
 
-var Item = function(name, price, quantity) {
-    this.name = name, 
-    this.price = price, 
-    this.quantity = quantity
-    };
+// item to select
+function Item(name, price, qty) {
+    this.name = name
+    this.price = price
+    this.qty = qty
+};
 
-// add Item to cart (name, price, quantity)
-function addItemToCart(name, price, quantity) { 
+// Add item to cart
+function addItemToCart(name, price, qty) {
     for (var i in cart) {
         if (cart[i].name === name) {
-            cart[i].quantity += quantity;
+            cart[i].qty += qty;
             return;
         }
     }
-    var item = new Item(name, price, quantity);
+    var item = new Item(name, price, qty);
     cart.push(item);
-};
+}
 
-addItemToCart('Nike', 100.99, 1);
-addItemToCart('Addidas', 74.99, 1);
-addItemToCart('Shoe String', 2.25, 2);
-addItemToCart('Socks', 4.25, 4);
+    addItemToCart("Snickers", 1.25, 1);
+    addItemToCart("Kisses", 1.00, 1);
+    addItemToCart("Payday", 1.15, 1);
 
-// remove one Item from cart (name)
-function removeItemFromCart (name) {
+// Delete item from cart
+function deleteItemFromCart(name) {
     for (var i in cart) {
         if (cart[i].name === name) {
-            cart[i].quantity --;
-            if (cart[i].quantity === 0) {
-                cart.splice(i, 1);
-            }
+            cart.splice(i, 1);
             break;
         }
     }
 }
 
-// count total quantity of Item in cart
-function quantityCart() {
-    var totalCount = 0;
-    for (var i in cart) {
-        totalCount += cart[i].quantity;
-    }
-    return totalCount;
+//console.log(cart);
+
+// clear all items from Cart
+function clearCart() {
+    cart = [];
 }
- console.log( quantityCart() );
 
- function totalCart() {
-     var totalCost = 0;
-     for (var i in cart) {
-         totalCost += cart[i].price;
-     }
-     return totalCost;
- }
- console.log( totalCart() );
+// cart qty and price total
+function qtyCart() {
+    var totalQty = 0;
+    for (var i in cart) {
+        totalQty += cart[i].qty;
+    }
+    return totalQty;
+}
 
+//console.log( qtyCart() );
 
+function totalCart() {
+    var totalPrice = 0;
+    for (var i in cart) {
+        totalPrice += cart[i].price;
+    }
+    return totalPrice;
+}
 
+//console.log( totalCart() );
 
+// Display Cart
+function listCart() { // array of Items
+    var cartCopy = [];
+    for (var i in cart) {
+        var item = cart[i];
+        var itemCopy = {};
+        for (var p in item) {
+            itemCopy[p] = item[p];
+        }
+        cartCopy.push(itemCopy);
+    }
+    return cartCopy;
+}
+
+var array = listCart();
+
+//console.log( array );
